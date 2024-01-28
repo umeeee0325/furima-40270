a874dee0dc89c79ed0f0152d77b00956fb334bf2
umeeee0325 <nana19970204@icloud.com>
2024-01-28 14:49:59 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は正しい形式で入力してください" }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は半角英数字の混合で入力してください" }
+  validates :password_confirmation, presence: true
+
 end
