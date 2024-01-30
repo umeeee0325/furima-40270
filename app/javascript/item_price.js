b4b1de8cecae699c27794c94c55d7e592e33d1ba
umeeee0325 <nana19970204@icloud.com>
2024-01-30 17:58:25 +0900

    販売手数料や販売利益の計算修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,12 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-     const inputValue = priceInput.value;
+     const inputValue = parseInt(priceInput.value);
      const addTaxDom = document.getElementById("add-tax-price");
      const profitDom = document.getElementById("profit");
 
-     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-     profitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+     addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toString();
+     profitDom.innerHTML = (inputValue - parseInt(addTaxDom.innerHTML)).toString();
   })
 });
 
