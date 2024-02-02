7768a941d7c30aebf3b6d5be1a4f8a482e0abde5
umeeee0325 <nana19970204@icloud.com>
2024-02-02 17:23:16 +0900

    updateアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 end
