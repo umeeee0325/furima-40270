5538a1f5004dd2d4da925521b19cc2476ce9ede9
umeeee0325 <nana19970204@icloud.com>
2024-02-05 12:08:52 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'items#index'
-  resources :items
+  resources :items do
+    resources :oders, only: [:create, :index]
+  end
 end
