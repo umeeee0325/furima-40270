c5236b913a04a4bc7b91089e5cffd88af8a54464
umeeee0325 <nana19970204@icloud.com>
2024-02-05 19:08:43 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,25 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @item = Item.find(params[:item_id])
+    @order = OrderResidence.new
   end
 
   def create
-    binding.pry
+    @order = OrderResidence.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_residence).permit(:postal_code, :region_id, :city, :house_number, :building_name, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
