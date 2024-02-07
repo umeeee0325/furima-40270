43a570ecc6648265c06fa6c7ed49a1fd3d3cb987
umeeee0325 <nana19970204@icloud.com>
2024-02-07 11:22:35 +0900

    商品一覧機能の追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :update, :edit, :destroy]
 
   def index
-    @item = Item.order('created_at DESC')
+    @items = Item.order('created_at DESC')
   end
 
   def new
